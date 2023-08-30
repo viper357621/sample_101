0df6fd7b2c9307c445192b9a543a74f3e7f67e41
lone dev <lonedev@gmail.com>
2023-08-30 22:46:55 +0800

    letn

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +17,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+    // $users = DB::select("select * from users");
+    //  $users = DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['Marc','me@bibi.com','password']);
+    //    $users = DB::table('users')->get();
+
+    // $users = DB::table('users')->insert([
+    //     'name' => 'aw',
+    //     'email' => 'aw@gmail.co',
+    //     'password' => 'aw@gmail.co',
+    // ]);
+
+    // $users = User::create([
+    // 'name' => 'aw',
+    // 'email' => 'aw@gmail.cos',
+    // 'password' => 'aw@gmail.cos',
+    // ]);
+    // $users = User::where('id', 2)->update(['email' => 'a@gmail.cow']);
+    // $users = DB::table('users')->where('id',2)->update(['email'=>'a@gmail.co']);
+
+    // $users = DB::table('users')->where('id',2)->update(['email'=>'a@gmail.co']);
+//    $users = User::where('id',1)->first();
+    // $users = User::find(2);
+    // $users->update([
+    //     'email'=>'aaa',
+    // ]);
+
+    // $users =User::find(2);
+    // $users->delete();
+
+    // $users = User::all();
+    // dd($users);
+   });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
